a0fe4b9a3d8729dda43ff9e91b7ad5431adc49d5
joffrey-gibouin <joffreygibouin@gmail.com>
2022-01-31 19:31:11 +0100

    add register form

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -10,17 +10,17 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class LoginController extends AbstractController
 {
     /**
-     * @Route("/login", name="login")
+     * @Route("/security", name="security")
      */
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        // get the login error if there is one
+        // get the security error if there is one
                  $error = $authenticationUtils->getLastAuthenticationError();
 
                  // last username entered by the user
                  $lastUsername = $authenticationUtils->getLastUsername();
 
-          return $this->render('login/index.html.twig', [
+          return $this->render('security/index.html.twig', [
                            'last_username' => $lastUsername,
                            'error'         => $error,
           ]);
